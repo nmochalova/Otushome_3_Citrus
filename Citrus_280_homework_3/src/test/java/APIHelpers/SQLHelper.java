6dff2806f06dcbb2226c8b29a3225834331e3b56
NatalyM <nbelokopytova@gmail.com>
2022-09-26 13:06:03 +0300

    sql helper (комментарии)

--- Citrus_280_homework_3/src/test/java/APIHelpers/SQLHelper.java
+++ Citrus_280_homework_3/src/test/java/APIHelpers/SQLHelper.java
@@ -8,6 +8,18 @@ import org.testng.annotations.Test;
 
 import javax.sql.DataSource;
 
+/*
+Установка docker-образа с postrgres
+https://hub.docker.com/_/postgres
+
+docker pull postgres
+docker run -p 5432:5432 --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+
+Создание таблицы
+create table test_table (integer id);
+insert into test_table(id) values (1);
+ */
+
 public class SQLHelper extends TestNGCitrusTestRunner {
     @Autowired
     public DataSource sqlHelper;
