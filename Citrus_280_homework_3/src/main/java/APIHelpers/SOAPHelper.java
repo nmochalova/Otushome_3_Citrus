3f9fe57acc27e494bc6f289ce54e74e3c37cd398
NatalyM <nbelokopytova@gmail.com>
2022-09-29 19:19:57 +0300

    Рефакторинг SOAPHelper

---
+++ Citrus_280_homework_3/src/main/java/APIHelpers/SOAPHelper.java
@@ -0,0 +1,59 @@
+package APIHelpers;
+
+import com.consol.citrus.dsl.design.AbstractTestBehavior;
+import features.PojoToXML;
+import pojo.xml.com.dataaccess.webservicesserver.NumberToDollars;
+import pojo.xml.com.dataaccess.webservicesserver.NumberToDollarsResponse;
+
+import java.math.BigDecimal;
+
+public class SOAPHelper extends AbstractTestBehavior {
+  private String num;
+  private String dollars;
+
+  public SOAPHelper(String num, String dollars) {
+    this.num = num;
+    this.dollars = dollars;
+  }
+
+  @Override
+  public void apply() {
+    PojoToXML<Class<NumberToDollars>> ptxRq = new PojoToXML<>(); //ptxRq - pojo to xml request
+    PojoToXML<Class<NumberToDollarsResponse>> ptxRs = new PojoToXML<>(); //ptxRq - pojo to xml response
+
+    //Отправка soap-сообщения с использованием самописного маршелера PojoToXML
+    soap()
+            .client("soapClient")
+            .send()
+            .payload(ptxRq.convert(NumberToDollars.class, getNumberToDollarsRequest(),
+                    "http://www.dataaccess.com/webservicesserver/",
+                    "NumberToDollars")
+    );
+
+    //Получение soap-ответа и сравнение его с эталонным xml
+    soap()
+            .client("soapClient")
+            .receive()
+            .xsdSchemaRepository("schemaRepositoryService")
+            .payload(ptxRs.convert(NumberToDollarsResponse.class, getNumberToDollarsResponse(),
+                    "http://www.dataaccess.com/webservicesserver/",
+                    "NumberToDollarsResponse")
+    );
+  }
+
+  //Метод который возвращает эталонный pojo-объект NumberToDollars для сравнения c полем Num=15
+  //Для генерации pojo на основе wsdl необходимо в pom.xml подключить плагин cxf-codegen-plugin
+  //и далее из терминала сгенерить pojo командой: mvn generate-source
+  public NumberToDollars getNumberToDollarsRequest() {
+    NumberToDollars numberToDollars = new NumberToDollars();
+    numberToDollars.setDNum(new BigDecimal(this.num));
+    return numberToDollars;
+  }
+
+  //Метод который возвращает эталонный pojo-объект NumberToDollarsResponse для сравнения c ответом "fifteen dollars"
+  public NumberToDollarsResponse getNumberToDollarsResponse() {
+    NumberToDollarsResponse numberToDollarsResponse = new NumberToDollarsResponse();
+    numberToDollarsResponse.setNumberToDollarsResult(this.dollars);
+    return numberToDollarsResponse;
+  }
+}
--- Citrus_280_homework_3/src/test/java/APIHelpers/SOAPHelperTest.java
+++ Citrus_280_homework_3/src/test/java/APIHelpers/SOAPHelperTest.java
@@ -2,6 +2,7 @@ package APIHelpers;
 
 import com.consol.citrus.annotations.CitrusTest;
 import com.consol.citrus.context.TestContext;
+import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
 import com.consol.citrus.dsl.testng.TestNGCitrusTestRunner;
 import features.PojoToXML;
 import org.testng.annotations.Test;
@@ -10,50 +11,25 @@ import pojo.xml.com.dataaccess.webservicesserver.NumberToDollarsResponse;
 
 import java.math.BigDecimal;
 
-public class SOAPHelperTest extends TestNGCitrusTestRunner {
+public class SOAPHelperTest extends TestNGCitrusTestDesigner {
     public TestContext context;
 
     @Test(description = "SOAP тест: число 20", enabled = true)
     @CitrusTest
     public void getTestActions() {
         this.context = citrus.createTestContext();
+        //SOME STEPS
+        //...
 
-        PojoToXML<Class<NumberToDollars>> ptxRq = new PojoToXML<>(); //ptxRq - pojo to xml request
-        PojoToXML<Class<NumberToDollarsResponse>> ptxRs = new PojoToXML<>(); //ptxRq - pojo to xml response
+        //HTTP HELPER. Check convert number to dollars
+        String num = "10";                  //что конвертируем
+        String dollars = "ten dollars";     //ожидаемый результат
+        applyBehavior(new SOAPHelper(num,dollars));
 
-        //Отправка soap-сообщения с использованием самописного маршелера PojoToXML
-        soap(soapActionBuilder -> soapActionBuilder
-                .client("soapClient")
-                .send()
-                .payload(ptxRq.convert(NumberToDollars.class, getNumberToDollarsRequest(),
-                        "http://www.dataaccess.com/webservicesserver/",
-                        "NumberToDollars"))
-        );
+        //SOME STEPS
+        //...
 
-        //Получение soap-ответа и сравнение его с эталонным xml
-        soap(soapActionBuilder -> soapActionBuilder
-                .client("soapClient")
-                .receive()
-                .xsdSchemaRepository("schemaRepositoryService")
-                .payload(ptxRs.convert(NumberToDollarsResponse.class, getNumberToDollarsResponse(),
-                        "http://www.dataaccess.com/webservicesserver/",
-                        "NumberToDollarsResponse"))
-        );
     }
 
-    //Метод который возвращает эталонный pojo-объект NumberToDollars для сравнения c полем Num=15
-    //Для генерации pojo на основе wsdl необходимо в pom.xml подключить плагин cxf-codegen-plugin
-    //и далее из терминала сгенерить pojo командой: mvn generate-source
-    public NumberToDollars getNumberToDollarsRequest() {
-        NumberToDollars numberToDollars = new NumberToDollars();
-        numberToDollars.setDNum(new BigDecimal("20"));
-        return numberToDollars;
-    }
 
-    //Метод который возвращает эталонный pojo-объект NumberToDollarsResponse для сравнения c ответом "fifteen dollars"
-    public NumberToDollarsResponse getNumberToDollarsResponse() {
-        NumberToDollarsResponse numberToDollarsResponse = new NumberToDollarsResponse();
-        numberToDollarsResponse.setNumberToDollarsResult("twenty dollars");
-        return numberToDollarsResponse;
-    }
 }
